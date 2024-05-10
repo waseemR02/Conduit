73597612d9a181d96b6967fd8665018a15970c71
waseemR02 <waseem.riaz.9999@gmail.com>
2024-05-10 16:15:24 +0530

    Remove database and instead serve a rest api for grafana
    Signed-off-by: waseemR02 <waseem.riaz.9999@gmail.com>

--- conduit/main.py
+++ conduit/main.py
@@ -1,33 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
-import sqlite3
 from datetime import datetime
-
+from flask import Flask, jsonify
 
 class Conduit:
-    def __init__(self):
-        self.conn = sqlite3.connect('/home/news.db')
-        self.c = self.conn.cursor()
-        self.c.execute('CREATE TABLE IF NOT EXISTS news (title TEXT, source Text, time INTEGER(11))')
-        self.conn.commit()
-
-
-    def write_to_db(self, data: list):
-        c = self.conn.cursor()
-        c.executemany('INSERT INTO news VALUES (?,?,?)', data)
-        self.conn.commit()
-
-    def poll(self):
+    def poll():
         rss_feed = requests.get("https://news.google.com/rss?hl=en-IN&gl=IN&ceid=IN:en")
         rss_text = rss_feed.text
         soup = BeautifulSoup(rss_text, 'xml')
         items = soup.findAll('item')
 
-        data = [(item.title.text, item.source.text, datetime.now().timestamp()) for item in items]
+        data = {
+            "data": [
+            {
+                "title": item.title.text,
+                "source": item.source.text,
+                "timestamp": datetime.now().timestamp()
+            }
+            for item in items
+            ]
+        }
         
-        self.write_to_db(data)
+        return data
 
+app = Flask(__name__)
+conduit = Conduit()
+
+@app.route('/poll', methods=['GET'])
+def respond():
+    data = conduit.poll()
+    return jsonify(data)
 
 if __name__ == '__main__':
-    conduit = Conduit()
-    conduit.poll()
+    app.run()
