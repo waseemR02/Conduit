bc8470d2f291423438d79c20127d8beec6d509dc
waseemR02 <waseem.riaz.9999@gmail.com>
2024-09-10 11:21:55 +0530

    remove class and add func endpoints
    Signed-off-by: waseemR02 <waseem.riaz.9999@gmail.com>

--- conduit/main.py
+++ conduit/main.py
@@ -1,37 +1,49 @@
 from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
-from flask import Flask, jsonify
-
-
-class Conduit:
-    def poll(arg):
-        rss_feed = requests.get("https://news.google.com/rss?hl=en-IN&gl=IN&ceid=IN:en")
-        rss_text = rss_feed.text
-        soup = BeautifulSoup(rss_text, "xml")
-        items = soup.findAll("item")
-
-        data = {
-            "data": [
-                {
-                    "title": item.title.text,
-                    "source": item.source.text,
-                    "timestamp": datetime.now().timestamp(),
-                }
-                for item in items
-            ]
-        }
-
-        return data
+from flask import Flask, jsonify, Response
+import json
 
 
 app = Flask(__name__)
 
 
-@app.route("/poll", methods=["GET"])
-def respond():
-    conduit = Conduit()
-    data = conduit.poll()
+@app.route("/sprintlist", methods=["GET"])
+def sprintlist() -> Response:
+    with open("docs/SprintsDump.json") as f:
+        return jsonify(json.load(f))
+
+
+@app.route("/4136d780-1d04-4709-a4b5-81f89e471ec6", methods=["GET"])
+def sprintdata() -> Response:
+    with open("docs/Sprint86Dump.json") as f:
+        return jsonify(json.load(f))
+
+
+@app.route("/parentTask/2539400", methods=["GET"])
+def parentTask() -> Response:
+    with open("docs/ParentTask1Sprint86Dump.json") as f:
+        return jsonify(json.load(f))
+
+
+@app.route("/rss", methods=["GET"])
+def rss() -> Response:
+    rss_feed = requests.get("https://news.google.com/rss?hl=en-IN&gl=IN&ceid=IN:en")
+    rss_text = rss_feed.text
+    soup = BeautifulSoup(rss_text, "xml")
+    items = soup.findAll("item")
+
+    data = {
+        "data": [
+            {
+                "title": item.title.text,
+                "source": item.source.text,
+                "timestamp": datetime.now().timestamp(),
+            }
+            for item in items
+        ]
+    }
+
     return jsonify(data)
 
 
